8d326487e7876e26e2890ab6547652e5c4f68887
diegor054 <thebrickwall054@gmail.com>
2023-06-05 21:51:45 -0700

    added post function for messages

---
+++ back/routes/messages.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const User = require('../model/messages');
+const router = express.Router()
+
+module.exports = router;
+
+router.post('/messages',  async (req, res)=>{
+    const {username, room, message} = req.body;
+    const newMessage = new Messages ({
+        sender: username,
+        room: room,
+        message: message,
+    })
+    console.log(newMessage + "created")
+    try{
+        const dataSaved = await newMessage.save();
+        res.status(200).json({dataSaved, status: 200});
+    }
+    catch (error){
+        console.log(error);
+        res.send("ERROR!");
+    }
+  })
