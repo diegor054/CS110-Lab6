619d1a6081f88248b47bc6d9f0dcc4e924e72642
zdamm001 <zdamm001@ucr.edu>
2023-06-06 01:24:34 -0700

    fixed merge conflict

--- front/src/Screens/Chatroom.js
+++ front/src/Screens/Chatroom.js
@@ -25,25 +25,6 @@ class Chatroom extends react.Component{
       });
       
     }
-    
-    componentDidMount = (data) => {
-      console.log("chatroom mount: ", this.props.room, this.props.username); 
-      this.socket.emit("join", {"room": this.props.room, "username": this.props.username});
-       //get messages from database
-    fetch(this.props.server_url + '/api/messages/' + `${this.props.room}`, {
-      method: "GET",
-      credentials: "include",
-      headers: {
-          "Content-Type": "application/json",
-      },
-      }).then((res) => {
-      console.log("pulled messages from db");
-      res.json().then(data => {
-          this.setState({messages: data})
-          //this.setState({rooms:data, username: this.props.username}); 
-      });
-  });
-    };
 
     handleMessageChange = (event) => {
       this.setState({ message: event.target.value });
@@ -113,6 +94,40 @@ class Chatroom extends react.Component{
       // Start the initial polling request
       checkForNewMessages();
     }
+
+    handleSendMessage = () => {
+      const { message } = this.state;
+      const {username, room} = this.props;
+      const data = {
+        username: username,
+        room: room,
+        message: message
+      }
+      this.socket.emit("chat message", data);
+      this.setState({ message: "" });
+      fetch(this.props.server_url + '/api/messages', {
+        method: "POST",
+        mode: "cors",
+        credentials: "include",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify(data),
+    }).then((res) => {
+        res.json().then((data) => {
+            console.log(data.status)
+            if (data.status === 200) {
+                //alert("Account created");
+                //this.props.changeScreen("lobby");
+            }
+            else {
+                console.log("failed to send message to database")
+                alert(data.msg);
+            }
+        });
+    });
+    };
   
     goBack = () => {
       this.props.changeScreen("lobby");
