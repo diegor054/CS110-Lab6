1e14095f6b584da06d681d3e19cbe1b87a0f28ef
diegor054 <thebrickwall054@gmail.com>
2023-06-05 22:25:04 -0700

    changed schema for messages

--- back/model/messages.js
+++ back/model/messages.js
@@ -6,14 +6,16 @@ const messageSchema = mongoose.Schema(
       text: { type: String, required: true },
     },
     sender: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
-      required: true,
+     // type: mongoose.Schema.Types.ObjectId,
+      //ref: "User",
+      //required: true,
+      text: { type: String, required: true },
     },
     room: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Room",
-      required: true,
+      //type: mongoose.Schema.Types.ObjectId,
+      //ref: "Room",
+      //required: true,
+      text: { type: String, required: true },
     },
   },
   {
