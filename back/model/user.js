18d52058693c6934c40207234f5601f90e6cf3f0
diegor054 <thebrickwall054@gmail.com>
2023-06-12 00:15:56 -0700

    fixed room bug

--- back/model/user.js
+++ back/model/user.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({  
-    //email: {
-         //   required: true,
-          //  type: String
-    //},
+    email: {
+            required: true,
+           type: String
+    },
     password: {
         required: true,
         type: String
